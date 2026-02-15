4374cee4e9ea37436b604fc12e51c08e20ef0a12
The App Artisan <theappartisan.dev@gmail.com>
2026-02-15 10:38:02 +0400

    Create a basic express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello!');
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Feedback Form Express Backend',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
